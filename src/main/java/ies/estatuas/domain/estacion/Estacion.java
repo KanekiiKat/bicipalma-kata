fba1f4dce98a32bc53133ba328cb64755f52adb7
Pistacho14 <pablogonzaleztemes@gmail.com>
2025-02-19 20:18:10 +0100

    feature(Estacion.java): :sparkles: añadir nombres de métodos

--- src/main/java/ies/estatuas/domain/estacion/Estacion.java
+++ src/main/java/ies/estatuas/domain/estacion/Estacion.java
@@ -1,4 +1,62 @@
 package ies.estatuas.domain.estacion;
 
 public class Estacion {
+
+    private int id;
+    private String direccion;
+    int anclajes
+
+    public Estacion(int id, String direccion int anclajes) {
+        this.id = id;
+        this.direccion = direccion;
+        this.anclajes = anclajes;
+    }
+
+    private int getId() {
+        return this.id;
+    }
+
+    private String getDireccion() {
+        return this,direccion;
+    }
+
+    public String toString() {
+        return Estacion
+    }
+
+    private int numAnclajes() {
+
+    }
+
+    public void consultarEstacion() {
+
+    }
+
+    public int anclajesLibres() {
+
+    }
+
+    public void anclarBicicleta(Movil) {
+
+    }
+
+    public boolean leerTarjetaUsuario(Autenticacion) {
+
+    }
+
+    public void retirarBicicleta(Autenticacion) {
+
+    }
+
+    private void mostrarBicicleta(Movil, int id) {
+
+    }
+
+    private void mostrarAnclaje(Movil, int anclajes) {
+
+    }
+
+    public void consultarAnclajes() {
+
+    }
 }
