f3708b41aac1b0ef5fdb07e033393fb4f7eb7e9c
Pistacho14 <pablogonzaleztemes@gmail.com>
2025-02-19 20:20:54 +0100

    feature(Bicicleta.java): :sparkles: añadir metodos

--- src/main/java/ies/estatuas/domain/bicicleta/Bicicleta.java
+++ src/main/java/ies/estatuas/domain/bicicleta/Bicicleta.java
@@ -1,4 +1,14 @@
 package ies.estatuas.domain.bicicleta;
 
 public class Bicicleta {
+
+    private final int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public String toString() {
+
+    }
 }
