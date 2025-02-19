1618929dab0ebf5feb9ae96d110aa5d1be95e607
Pistacho14 <pablogonzaleztemes@gmail.com>
2025-02-19 18:52:48 +0100

    feature(TarjetaUsuario.java) :sparkles: add class TarjetaUsuario.java

--- src/main/java/ies/estatuas/domain/tarjetausuario/TarjetaUsuario.java
+++ src/main/java/ies/estatuas/domain/tarjetausuario/TarjetaUsuario.java
@@ -1,4 +1,19 @@
 package ies.estatuas.domain.tarjetausuario;
 
 public class TarjetaUsuario {
+
+    private final String id;
+    private boolean activada ;
+
+    public boolean isActivada(){
+        return activada;
+    }
+
+    public void setActivada(boolean activada){
+        this.activada = activada;
+    }
+
+    public String toString() {
+        return id;
+    }
 }
